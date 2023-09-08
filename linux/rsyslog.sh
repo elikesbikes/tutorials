f6fab7143a1fd4b35bd5c082271fc441a6ef5c92
ELIKESBIKES <ecloaiza@users.noreply.github.com>
2023-09-07 17:31:47 -0700

    repo

--- linux/rsyslog.sh
+++ linux/rsyslog.sh
@@ -1,1 +1,11 @@
+yum install rsyslog
+echo "*.* @192.168.5.20:1514" >> /etc/rsyslog.conf 2>&1
+echo "*.* @192.168.5.20:1514" >> /etc/rsyslog.conf 2>&1
+cat /etc/rsyslog.conf | grep 192.168
+systemctl restart rsyslog 2>&1
 
+
+
+
+
+apk add openrc busybox-initscripts
